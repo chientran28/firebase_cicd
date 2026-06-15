42528c9096163e8d75e8a992e01ab8754413f329
chientran2311 <chientd@newwave.com.vn>
2026-06-15 16:23:55 +0700

    fix cicd ,then ui env expresion

--- lib/main.dart
+++ lib/main.dart
@@ -88,9 +88,14 @@ class _MyHomePageState extends State<MyHomePage> {
         // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
         // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
+        bottom: PreferredSize(
+          preferredSize: const Size.fromHeight(20),
+          child: Text(
+            '${dotenv.env['ENVIRONMENT']} | ${dotenv.env['API_BASE_URL']}',
+            style: const TextStyle(fontSize: 11),
+          ),
+        ),
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
